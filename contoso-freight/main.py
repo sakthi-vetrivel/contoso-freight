2df6480cdff7d6588ace60556e486ef07e49c819
Sakthi Vetrivel <suvetriv@microsoft.com>
2019-10-24 14:16:41 -0700

    adding all files to containerize

--- contoso-freight/main.py
+++ contoso-freight/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/index', methods=['GET', 'POST'])
 def homepage():
-    return render_template("index.html")
+    return render_template("new_index.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=9090, debug=True)
