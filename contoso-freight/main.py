b359b04c993cda745192fe74f25c0b86f854d4f3
Sakthi Vetrivel <suvetriv@microsoft.com>
2019-10-29 20:25:37 -0700

    tying my laces

--- contoso-freight/main.py
+++ contoso-freight/main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
-    return render_template("new_index.html")
+def homepage(): .
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=False)
