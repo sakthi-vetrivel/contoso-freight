7007e30003d79557ae712e17d9f453db706086d1
Sakthi Vetrivel <suvetriv@microsoft.com>
2019-10-29 19:07:55 -0700

    updating UI

--- contoso-freight/main.py
+++ contoso-freight/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("index.html")
+    return render_template("new_index.html")
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80, debug=False)
